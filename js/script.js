89d5d8ff73ff452cafb7e7e2d9f5650ac7a1aeb7
lardam <felicitasgoyaprieto@MacBook-Air-de-Felicitas.local>
2022-02-05 10:18:52 -0300

    First stage almost finished - needs more styling and to start scripÂt.js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//Set default for display
+
+let input = document.getElementById('input')
+let result = document.getElementById('result')
+result.innerText = 0;
+
+let firstOperand = ''
+let secondOperand = ''
+
+
+
+//Get all btns
+
+//Create function to start them' maths
+    //Get btn to show in input
+    //Make input work
+
