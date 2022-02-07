d389d8d96d91d649dde9c5c9c179b404f199c8bf
lardam <felicitasgoyaprieto@MacBook-Air-de-Felicitas.local>
2022-02-07 12:31:48 -0300

    feli says hiiii

--- js/script.js
+++ js/script.js
@@ -1,17 +1,40 @@
-//Set default for display
+const input = document.getElementById('input')
+const result = document.getElementById('result')
+input.innerText = ''
 
-let input = document.getElementById('input')
-let result = document.getElementById('result')
-result.innerText = 0;
+//Get buttons
 
-let firstOperand = ''
-let secondOperand = ''
+const numbers = document.querySelectorAll('.num-btn')
+const operators = document.querySelectorAll('.op-btn')
+const equal = document.getElementById('eq-btn')
+const decimal = document.getElementById('dec-btn')
+const del = document.getElementById('del')
+const cle = document.getElementById('cle')
 
+numbers.forEach(function(btns) {
+    btns.addEventListener('click', () => appendNumber(btns.textContent))
+})
 
+operators.forEach(function(btns) {
+    btns.addEventListener('click', () => appendOperand(btns.textContent))
+})
 
-//Get all btns
+del.addEventListener('click', deleteNum())
+cle.addEventListener('click', clear())
 
-//Create function to start them' maths
-    //Get btn to show in input
-    //Make input work
+function clear() {
+    input.textContent = ''
+    result.textContent = ''
+}
 
+function deleteNum() {
+    input.textContent--
+}
+
+function appendNumber(value) {
+    input.textContent += value
+}
+
+function appendOperand(value) {
+    input.textContent = value
+}
